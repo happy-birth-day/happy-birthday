a3095b303e4c5bc88f7c554d9a847f491270bd4c
happy-birth-day <zihuaxzh@outlook.com>
2023-03-04 14:15:44 +0800

    1st commit

---
+++ config.js
@@ -0,0 +1,32 @@
+var config = {
+    // 句子的数量任意
+    texts: [
+        "献给",      
+        "我心爱的宝宝",  
+        "今天是你的生日",
+        "这是我们在一起以后",
+        "第一次异国过生日",
+        "希望berber开开心心",
+        "愿你从今以后",
+        "生活明朗、万物可爱",
+        "Happy birthday!!!",
+        "希望berber早日回来",
+        "我发誓给你买狗！"
+    ],
+    
+    imgs: {
+        
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述
+    desc: {
+        turn_on: "欣欣公主! click here!",
+        play: "music!",
+        bannar_coming: "生日快乐呀！！！",
+        balloons_flying: "气球～～～",
+        cake_fadein: "记得吃蛋糕～",
+        light_candle: "记得点蜡烛～",
+        wish_message: "有好多话想给你说",
+        story: "它们已经迫不及待地要跑出来了～",
+    }
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# happy birthday
+
+Wishing you a happy birthday! 
+
+
+
+May the best and loving things be some of the joy your birthday brings！
+
+
+
+Yours，sincerely
+
+HuaHua
